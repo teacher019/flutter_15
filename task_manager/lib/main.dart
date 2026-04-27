9044e01c3a134ca10ac459381b35bc3ec47a4c6a
teacher019 <abdulkhalekt10@gmail.com>
2026-04-27 19:27:07 +0600

    task_manager

--- task_manager/lib/main.dart
+++ task_manager/lib/main.dart
@@ -1,53 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:task_manager/screens/splash_screen.dart';
-import 'package:task_manager/utils/app_color.dart';
-class TaskManagerApp extends StatelessWidget {
-  const TaskManagerApp({super.key});
+import 'package:flutter/cupertino.dart';
+import 'package:task_manager/app.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          textTheme: TextTheme(
-              titleLarge: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.w600
-              )
-          ),
-
-
-          inputDecorationTheme: InputDecorationTheme(
-              filled: true,
-              fillColor: Colors.white,
-
-              hintStyle: TextStyle(
-                  color: Colors.grey
-              ),
-
-              enabledBorder: OutlineInputBorder(
-                  borderSide:   BorderSide.none
-              ),
-
-              border: OutlineInputBorder(
-                  borderSide: BorderSide.none
-              )
-
-
-          ),
-
-          filledButtonTheme: FilledButtonThemeData(
-              style: FilledButton.styleFrom(
-                  backgroundColor: AppColor.Pcolor2,
-                  fixedSize: Size.fromWidth(double.maxFinite),
-                  padding: EdgeInsets.symmetric(vertical: 12),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8)
-                  )
-              )
-          )
-      ),
-      title: 'Task manager',
-      home: SplashScreen(),
-    );
-  }
+void main(){
+  runApp(TaskManagerApp());
 }
